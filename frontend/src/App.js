0ebd6e670480dd1b38c6642b9bc02e277c811f7d
YURIPADUA <yuripadua@gmail.com>
2025-01-16 02:30:52 -0300

    removing portuguese code/comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// import AnonymousChat from './pages/AnonymousChat';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import MainChat from './pages/MainChat'; // esse com sidebar etc.
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<AnonymousChat />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/app/*" element={<MainChat />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
