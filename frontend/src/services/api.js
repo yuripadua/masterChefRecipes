ef8281f50d0c9043fb4c58eb19353bd75d987646
YURIPADUA <yuripadua@gmail.com>
2025-01-16 03:13:52 -0300

    fix backend client

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
       ? 'http://localhost:5000'
-      : 'https://masterChefRecipesBackEnd.onrender.com'
+    //   : 'https://masterChefRecipesBackEnd.onrender.com'
+        : 'https://masterchefrecipes.onrender.com/'
   });
   
 
