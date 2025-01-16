7dc163df0ad06d08119c9332d41dc2305743c2eb
YURIPADUA <yuripadua@gmail.com>
2025-01-16 01:11:12 -0300

    cleaning all unused file on version 2

--- backend/src/models/Recipe.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-
-// const recipeSchema = new mongoose.Schema({
-//   title: String,
-//   ingredients: [String],
-//   instructions: String
-// });
-
-// module.exports = mongoose.model('Recipe', recipeSchema);
